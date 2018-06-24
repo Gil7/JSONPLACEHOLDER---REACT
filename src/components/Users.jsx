c89c24493cbf8316961ccd83baef01f8f142f536
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-06-24 18:08:16 -0500

    :+1: displaying users

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,13 +1,36 @@
 import React, { Component } from 'react'
-import User from '.User'
+import User from './User'
 class Users extends Component { 
+    state = {
+        users: []
+    }
     componentWillMount = () => {
         this.loadUsers()
     }
+    loadUsers = () => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(users => {
+            this.setState(
+                {
+                    users: users
+                }
+            )
+        })
+        .catch(err => console.log(err))
+    }
     render(){
+        const usersList = this.state.users.map(user => {
+            return (
+                <User
+                    name={user.name}
+                />
+            )
+        })
         return (
             <div>
                 <h2>Users created</h2>               
+                {usersList}
             </div>
         )
     }
