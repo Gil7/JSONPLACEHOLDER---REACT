c960a2c88d739d7d9373beddf02227fcb0c0cfec
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-06-24 19:51:03 -0500

    :+1: post details

--- src/components/PostDetails.jsx
+++ src/components/PostDetails.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-
+import { Link } from 'react-router-dom'
 class PostDetails extends Component {
     constructor(props){
         super(props)
@@ -7,10 +7,32 @@ class PostDetails extends Component {
     state = {
         post : {}
     }
+    componentWillMount = () => {
+        this.loadPost()
+    }
+    loadPost = () => {
+        const id = this.props.match.params.id
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        .then(res => res.json())
+        .then(post => {
+            this.setState({
+                post : post
+            })
+        })
+    }
     render(){
         return (
-            <div>
-                {this.props.params}
+            <div className="col s12 m6">
+                <div className="card blue-grey darken-1">
+                    <div className="card-content white-text">
+                        <span className="card-title"> - {this.state.post.title}</span>
+                        <p>{this.state.post.body}</p>
+                    </div>
+                    <div className="card-action">
+                        <Link to="/"  className="btn red"> Regresar</Link>
+                    </div>
+                </div>
+
             </div>
         )
     }
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -27,12 +27,12 @@ class Posts extends Component {
             return (
                 <div className="col s12 m6">
                     <div className="card blue-grey darken-1">
-                        <div class="card-content white-text">
+                        <div className="card-content white-text">
                             <span className="card-title">{post.title.substring(0,10)}</span>
-                            <p>Email: {post.body.substring(0,40)}</p>
+                            <p>{post.body.substring(0,40)}</p>
                         </div>
                         <div className="card-action">
-                            <Link to="/posts/:id" params={post.id}>Read</Link>
+                            <Link to={`posts/${post.id}`} params={post.id}>Read</Link>
                         </div>
                     </div>
                 </div>
