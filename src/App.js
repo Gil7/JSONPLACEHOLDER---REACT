8c6e1d81d66a3fcb5b3d3775e9250e18b99cd49e
Gil Méndez <gilberto.mendez.santiz@gmail.com>
2018-06-24 19:32:49 -0500

    :+1: router defined

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Users from './components/Users'
+import Posts from './components/Posts'
+import Error from './components/Error'
+import Navigation from './components/Navigation'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import PostDetails from './components/PostDetails';
 class App extends Component {
   render() {
     return (
@@ -10,7 +15,19 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Users/>
+        
+        <BrowserRouter>
+          <div>
+            <Navigation />
+            <Switch>
+              <Route path="/" component={Users} exact />
+              <Route path="/posts" component={Posts} exact />
+              <Route path="/posts/:id" component={PostDetails} />
+              <Route component={Error} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+        
       </div>
     );
   }
